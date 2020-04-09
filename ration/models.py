9d3d67c49ff3312a990d87fbe94cabaa6b9a44d2
Tarun Kumar <sinnytk@github.com>
2020-04-09 17:42:31 +0500

    Add date_created and date_updated to allocations

--- ration/models.py
+++ ration/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
-
+from django.utils import timezone
 class RationAllocation(models.Model):
     user = models.ForeignKey(
         get_user_model(),
@@ -10,3 +10,12 @@ class RationAllocation(models.Model):
     )
     cnic = models.CharField(max_length=13, validators=[RegexValidator(regex='^[\d]{13}$', message='CNIC can only be 13 digit number', code='nomatch')], primary_key=True)
     org_name = models.CharField(max_length=150, default='', blank=True)
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.localtime(timezone.now())
+        self.modified = timezone.localtime(timezone.now())
+        return super(RationAllocation, self).save(*args, **kwargs)
